511b69f597756ea24bc2c5a333b594f702bc81e3
KatrinaOstrom <katrinaostrom@gmail.com>
2021-10-21 13:44:16 -0400

    finished "more mutations" but has errors

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
 
+import { BrowserRouter } from 'react-router-dom'
+import { AUTH_TOKEN } from './constants'
+
+
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
@@ -16,25 +21,41 @@ const httpLink = createHttpLink({
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
-}) */
-
-const authLink = setContext((_, { headers }) => {
+}) 
+*/
+/*const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTYzMzM3OTIxMX0.HeuBGiu_2bwzIosaLK73693AbedF14EyCWpLo4KDAOg',
     }
   }
 });
+*/
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache()
-});
+})
+
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
+
 //registerServiceWorker()
